08447d7cc1fed7c56e86539624a6057f63602b60
Artem Morozov <morozoff234@yandex.ru>
2024-11-10 02:56:11 +0300

    Fix bugs

---
+++ migrations/versions/fee34ac4fcab_delete_user_id_from_lectureruser.py
@@ -0,0 +1,24 @@
+"""delete-user-id-from-lectureruser
+
+Revision ID: fee34ac4fcab
+Revises: 0fbda260a023
+Create Date: 2024-11-10 02:38:49.538788
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 'fee34ac4fcab'
+down_revision = '0fbda260a023'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_column('lecturer_user_comment', 'user_id')
+
+
+def downgrade():
+    op.add_column('lecturer_user_comment', sa.Column('user_id', sa.INTEGER(), autoincrement=False, nullable=False))
