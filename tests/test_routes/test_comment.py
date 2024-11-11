49046b5d4e9d15f6b93b2b721a7b495454c542e2
default <z4haroff.ivan@yandex.com>
2024-11-11 20:45:40 +0000

    fixtures+ tests

--- tests/test_routes/test_comment.py
+++ tests/test_routes/test_comment.py
@@ -13,12 +13,8 @@ url: str = '/comment'
 settings = get_settings()
 
 
-def test_create_comment(client, dbsession):
-    body = {"first_name": 'Иван', "last_name": 'Иванов', "middle_name": 'Иванович', "timetable_id": 0}
-    lecturer: Lecturer = Lecturer(**body)
-    dbsession.add(lecturer)
-    dbsession.commit()
-
+def test_create_comment(client, dbsession, comment, lecturer):
+    lecturer = lecturer()
     body = {
         "subject": "Физика",
         "text": "Хороший препод",
@@ -28,7 +24,6 @@ def test_create_comment(client, dbsession):
     }
     params = {"lecturer_id": lecturer.id}
     post_response = client.post(url, json=body, params=params)
-    print(post_response.json())
     assert post_response.status_code == status.HTTP_200_OK
     json_response = post_response.json()
     comment = Comment.query(session=dbsession).filter(Comment.uuid == json_response["uuid"]).one_or_none()
@@ -41,17 +36,14 @@ def test_create_comment(client, dbsession):
     assert user_comment is not None
     dbsession.delete(user_comment)
     dbsession.delete(comment)
-    dbsession.delete(lecturer)
     dbsession.commit()
+    params = {"lecturer_id": 1}
     post_response = client.post(url, json=body, params=params)
     assert post_response.status_code == status.HTTP_404_NOT_FOUND
 
 
-def test_post_bad_mark(client, dbsession):
-    body = {"first_name": 'Иван', "last_name": 'Иванов', "middle_name": 'Иванович', "timetable_id": 0}
-    lecturer: Lecturer = Lecturer(**body)
-    dbsession.add(lecturer)
-    dbsession.commit()
+def test_post_bad_mark(client, lecturer):
+    test_lecturer = lecturer()
 
     body = {
         "subject": "Физика",
@@ -60,76 +52,69 @@ def test_post_bad_mark(client, dbsession):
         "mark_freebie": -2,
         "mark_clarity": 0,
     }
-    params = {"lecturer_id": lecturer.id}
+    params = {"lecturer_id": test_lecturer.id}
     post_response = client.post(url, json=body, params=params)
     assert post_response.status_code == status.HTTP_400_BAD_REQUEST
-    dbsession.delete(lecturer)
-    dbsession.commit()
 
 
-def test_get_comment(client, dbsession):
-    body = {
-        "first_name": 'Иван',
-        "last_name": 'Иванов',
-        "middle_name": 'Иванович',
-        "timetable_id": 0,
-    }
-    lecturer: Lecturer = Lecturer(**body)
-    dbsession.add(lecturer)
-    dbsession.commit()
-
-    body = {
-        "lecturer_id": lecturer.id,
-        "subject": "Физика",
-        "text": "Хороший препод",
-        "mark_kindness": 1,
-        "mark_freebie": -2,
-        "mark_clarity": 0,
-        "review_status": ReviewStatus.APPROVED,
-    }
-    comment: Comment = Comment(**body)
-    dbsession.add(comment)
-    dbsession.commit()
-    response_comment = client.get(f'{url}/{comment.uuid}')
-    assert response_comment.status_code == status.HTTP_200_OK
+def test_get_comment(client, dbsession, lecturer, comment):
+    test_lecturer = lecturer()
+    test_comment = comment(lecturer_id=test_lecturer.id)
     random_uuid = uuid.uuid4()
     response = client.get(f'{url}/{random_uuid}')
     assert response.status_code == status.HTTP_404_NOT_FOUND
-    comment = Comment.query(session=dbsession).filter(Comment.uuid == response_comment.json()["uuid"]).one_or_none()
-    assert comment is not None
-    dbsession.delete(comment)
-    dbsession.delete(lecturer)
-    dbsession.commit()
-
-
-def test_delete_comment(client, dbsession):
-    body = {"first_name": 'Иван', "last_name": 'Иванов', "middle_name": 'Иванович', "timetable_id": 0}
-    lecturer: Lecturer = Lecturer(**body)
-    dbsession.add(lecturer)
-    dbsession.commit()
-
-    body = {
-        "lecturer_id": lecturer.id,
-        "subject": "Физика",
-        "text": "Хороший препод",
-        "mark_kindness": 1,
-        "mark_freebie": -2,
-        "mark_clarity": 0,
-        "review_status": ReviewStatus.APPROVED,
-    }
-    comment: Comment = Comment(**body)
-    dbsession.add(comment)
-    dbsession.commit()
-    response = client.delete(f'{url}/{comment.uuid}')
+    response = client.get(f'{url}/{test_comment.uuid}')
     assert response.status_code == status.HTTP_200_OK
-    response = client.get(f'{url}/{comment.uuid}')
+    comment1 = Comment.query(session=dbsession).filter(Comment.uuid == response.json()["uuid"]).one_or_none()
+    assert comment1 is not None
+
+
+def test_get_comments_by_lecturer_id(client, lecturer, comment):
+    lecturer1 = lecturer()
+    lecturer2 = lecturer()
+    comment1_1 = comment(lecturer_id=lecturer1.id)
+    comment1_2 = comment(lecturer_id=lecturer1.id)
+    comment2_1 = comment(lecturer_id=lecturer2.id)
+    query = {"lecturer_id": lecturer1.id}
+    response = client.get(f'{url}', params=query)
+    assert response.status_code == status.HTTP_200_OK
+    json_response = response.json()
+    assert json_response["total"] == 2
+    query = {"lecturer_id": lecturer2.id}
+    response = client.get(f'{url}', params=query)
+    assert response.status_code == status.HTTP_200_OK
+    json_response = response.json()
+    assert json_response["total"] == 1
+    query = {"lecturer_id": 2}
+    response = client.get(f'{url}', params=query)
+    assert response.status_code == status.HTTP_200_OK
+    json_response = response.json()
+    assert json_response["total"] == 0
+
+
+def test_get_comments_unreviewed(client, lecturer, comment):
+    lecturer1 = lecturer()
+    lecturer2 = lecturer()
+    comment1_1 = comment(lecturer_id=lecturer1.id, review_status=ReviewStatus.APPROVED)
+    comment1_2 = comment(lecturer_id=lecturer1.id, review_status=ReviewStatus.PENDING)
+    comment2_1 = comment(lecturer_id=lecturer2.id, review_status=ReviewStatus.PENDING)
+    comment2_2 = comment(lecturer_id=lecturer2.id, review_status=ReviewStatus.DISMISSED)
+    query = {'unreviewed': True}
+    response = client.get(f'{url}', params=query)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_delete_comment(client, dbsession, lecturer, comment):
+    test_lecturer = lecturer()
+    test_comment = comment(lecturer_id=test_lecturer.id)
+    response = client.delete(f'{url}/{test_comment.uuid}')
+    dbsession.refresh(test_comment)
+    assert response.status_code == status.HTTP_200_OK
+    response = client.get(f'{url}/{test_comment.uuid}')
     assert response.status_code == status.HTTP_404_NOT_FOUND
     random_uuid = uuid.uuid4()
     response = client.delete(f'{url}/{random_uuid}')
     assert response.status_code == status.HTTP_404_NOT_FOUND
-    comment1 = Comment.query(session=dbsession).filter(Comment.uuid == comment.uuid).one_or_none()
+    comment1 = Comment.query(session=dbsession).filter(Comment.uuid == test_comment.uuid).one_or_none()
     assert comment1 is None
-    comment.is_deleted = True
-    dbsession.delete(comment)
-    dbsession.delete(lecturer)
-    dbsession.commit()
+    assert test_comment.is_deleted
