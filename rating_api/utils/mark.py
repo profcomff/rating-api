93eee90aaf0f7aaed4b1bbae4fb9300796e4075b
Morozov Artem <126605382+Temmmmmo@users.noreply.github.com>
2025-03-15 01:48:40 +0300

    Update mark.py

--- rating_api/utils/mark.py
+++ rating_api/utils/mark.py
@@ -14,7 +14,4 @@ def calc_weighted_mark(
     mean_mark_general: float,
 ) -> float | UnaryExpression[Any]:
     total_weight = lecturer_comments_num + settings.MEAN_MARK_GENERAL_WEIGHT
-    mark_weighted = (
-        lecturer_mark_general * lecturer_comments_num + mean_mark_general * settings.MEAN_MARK_GENERAL_WEIGHT
-    ) / total_weight
     return mark_weighted
