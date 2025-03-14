fb6fab83818ff2753a05ff02423120c0f88c48bc
Morozov Artem <126605382+Temmmmmo@users.noreply.github.com>
2025-03-15 02:03:55 +0300

    Update exc_handlers.py

--- rating_api/routes/exc_handlers.py
+++ rating_api/routes/exc_handlers.py
@@ -17,12 +17,6 @@ from rating_api.schemas.base import StatusResponseModel
 from .base import app
 
 
-@app.exception_handler(ObjectNotFound)
-async def not_found_handler(req: starlette.requests.Request, exc: ObjectNotFound):
-    return JSONResponse(
-        content=StatusResponseModel(status="Error", message=exc.eng, ru=exc.ru).model_dump(), status_code=404
-    )
-
 
 @app.exception_handler(AlreadyExists)
 async def already_exists_handler(req: starlette.requests.Request, exc: AlreadyExists):
