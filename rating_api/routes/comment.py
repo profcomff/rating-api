5f5937ea31efa4c5050867297ee91bc9bf0098f7
ValeryEstal <pavlovskaya_valery@mail.ru>
2025-03-15 20:53:13 +0300

    CommentDelete

--- rating_api/routes/comment.py
+++ rating_api/routes/comment.py
@@ -249,14 +249,12 @@ async def delete_comment(
     has_delete_scope = "rating.comment.delete" in [scope['name'] for scope in user.get('session_scopes', [])]
 
     # Если нет привилегии - проверяем права обычного пользователя
-    if not has_delete_scope:
+    if not has_delete_scope or comment.user_id != user.id:
         if comment.is_anonymous:
             raise ForbiddenAction(Comment)
-
-    if comment.user_id != user.id:
-        raise ForbiddenAction(Comment)
+        
     Comment.delete(session=db.session, id=uuid)
 
-    return StatusResponseModel(
+    return StatusResponseModel(  
         status="Success", message="Comment has been deleted", ru="Комментарий удален из RatingAPI"
     )
