74c5df2d38a93d349d3536369eb9b3f6a2ec8c37
Morozov Artem <126605382+Temmmmmo@users.noreply.github.com>
2025-03-15 12:54:33 +0300

    Update base.py

--- rating_api/schemas/base.py
+++ rating_api/schemas/base.py
@@ -4,8 +4,6 @@ from pydantic import BaseModel, ConfigDict
 class Base(BaseModel):
     def __repr__(self) -> str:
         attrs = []
-        for k, v in self.__class__.model_json_schema().items():
-            attrs.append(f"{k}={v}")
         return "{}({})".format(self.__class__.__name__, ', '.join(attrs))
 
     model_config = ConfigDict(from_attributes=True)
