5976ae928fa6ec820859216af738a2f5605c9827
Morozov Artem <126605382+Temmmmmo@users.noreply.github.com>
2025-03-15 16:15:33 +0300

    Update exceptions.py

--- rating_api/exceptions.py
+++ rating_api/exceptions.py
@@ -19,17 +19,12 @@ class ObjectNotFound(RatingAPIError):
         )
 
 
-class AlreadyExists(RatingAPIError):
-    def __init__(self, obj: type, obj_id_or_name: int | str):
-        super().__init__(
-            f"Object {obj.__name__}, {obj_id_or_name=} already exists",
-            f"Объект {obj.__name__} с идентификатором {obj_id_or_name=} уже существует",
-        )
 
 
 class TooManyCommentRequests(RatingAPIError):
     frequency: int
     limit: int
+    total: int = 5
 
     def __init__(self, frequency: int, limit: int):
         self.frequency = frequency
