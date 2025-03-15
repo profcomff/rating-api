d7fb2394e3bddf1ed2b8dcf9a8e5e4b23eb0dedc
Morozov Artem <126605382+Temmmmmo@users.noreply.github.com>
2025-03-15 16:48:36 +0300

    Update comment.py

--- rating_api/routes/comment.py
+++ rating_api/routes/comment.py
@@ -62,8 +62,8 @@ async def create_comment(lecturer_id: int, comment_info: CommentPost, user=Depen
         if len(comment_info.text) > settings.MAX_COMMENT_LENGTH:
             raise CommentTooLong(settings.MAX_COMMENT_LENGTH)
 
-        if re.search(r"^[a-zA-Zа-яА-Я\d!?,_\-.\"\'\[\]{}`~<>^@#№$%;:&*()+=\\\/ \n]*$", comment_info.text) is None:
-            raise ForbiddenSymbol()
+        # if re.search(r"^[a-zA-Zа-яА-Я\d!?,_\-.\"\'\[\]{}`~<>^@#№$%;:&*()+=\\\/ \n]*$", comment_info.text) is None:
+        #     raise ForbiddenSymbol()
 
     # Сначала добавляем с user_id, который мы получили при авторизации,
     # в LecturerUserComment, чтобы нельзя было слишком быстро добавлять комментарии
