22ca550b5b159a34f06a4a28437201127cb6f6c6
Artem Bratyashin <bratiashin.aa21@physics.msu.ru>
2025-11-18 23:39:04 +0300

    Formated migration file

--- migrations/versions/d322e8331f91_add_user_full_name_to_comment_table.py
+++ migrations/versions/d322e8331f91_add_user_full_name_to_comment_table.py
@@ -5,8 +5,9 @@ Revises: beb11fd89531
 Create Date: 2025-11-18 23:16:20.029675
 
 """
-from alembic import op
+
 import sqlalchemy as sa
+from alembic import op
 
 
 # revision identifiers, used by Alembic.
