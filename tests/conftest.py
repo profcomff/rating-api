f1280dbac999f55893bb180876620f6c8402d920
NamazovMaksim <aslimbo2001@gmail.com>
2026-07-05 00:38:36 +0300

    Удалена фикстура aiohttp_mp. Решено отказаться от aioressponses в пользу mocker, потому чтоэта библиотека не совместима с последними версиями aiohttp (==3.14.1)

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@ from pathlib import Path
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
-from aioresponses import aioresponses
 from alembic import command
 from alembic.config import Config as AlembicConfig
 from fastapi.testclient import TestClient
@@ -42,13 +41,6 @@ def session_mp():
     mp = MonkeyPatch()
     yield mp
     mp.undo()
-
-
-@pytest.fixture
-def aiohttp_mp():
-    """Фикстура для перехвата любых aiohttp запросов aiohttp.ClientSession()"""
-    with aioresponses() as aiohttp_mock:
-        yield aiohttp_mock
 
 
 @pytest.fixture(scope='session')
